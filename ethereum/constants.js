d2b3716bbd7812d41288073aa71eeb54d38dfd71
hexoul <crosien@gmail.com>
2018-10-08 16:56:30 +0900

    refactoring ethereum part to intialize web3.contract w/ github

---
+++ ethereum/constants.js
@@ -0,0 +1,21 @@
+let constants = {}
+constants.organization = 'hexoul'
+constants.repoName = 'poa-chain-spec'
+constants.addressesSourceFile = 'contracts.json'
+constants.ABIsSources = {
+  ClaimManager: 'ClaimManager.abi.json'
+}
+constants.NEW_MINING_KEY = {
+  label: 'New Mining Key',
+  lastNameAndKey: '',
+  fullName: '',
+  value: '0x0000000000000000000000000000000000000000'
+}
+constants.minBallotDurationInDays = 2
+constants.startTimeOffsetInMinutes = 5
+constants.endTimeDefaultInMinutes = 2890
+constants.getTransactionReceiptInterval = 5000
+constants.NETID_TEST = '101'
+module.exports = {
+  constants
+}
