c95fd45644bf74c7296c01d81c1cb77f04281ab3
hexoul <crosien@gmail.com>
2018-10-29 11:07:32 +0900

    ethereum: update abi file names

--- src/ethereum/constants.js
+++ src/ethereum/constants.js
@@ -3,8 +3,12 @@ constants.organization = 'hexoul';
 constants.repoName = 'poa-chain-spec';
 constants.addressesSourceFile = 'contracts.json';
 constants.ABIsSources = {
-  Identity: 'Identity.abi.json',
-  IdentityManager: 'IdentityManager.abi.json'
+  Identity: 'Identity.json',
+  IdentityManager: 'IdentityManager.json',
+  AttestationAgencyRegistry: 'AttestationAgencyRegistry.json',
+  TopicRegistry: 'TopicRegistry.json',
+  Achievement: 'Achievement.json',
+  AchievementManager: 'AchievementManager.json',
 };
 constants.NETID_TESTNET = '101';
 
